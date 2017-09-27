98dabf17173358ea6ff9c1cf8ef196381d1d14c3
Niall Coady <niall_coady7@hotmail.com>
2017-09-27 12:25:07 +0100

    Activity & Model updates

--- app/src/main/java/com/example/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/com/example/myrent/activities/ResidenceActivity.java
@@ -4,15 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 import com.example.myrent.R;
 import com.example.myrent.models.Residence;
 
-public class ResidenceActivity extends AppCompatActivity implements TextWatcher {
+public class ResidenceActivity extends AppCompatActivity implements TextWatcher, OnCheckedChangeListener {
 
-    private EditText geolocation;
+    private EditText  geolocation;
     private Residence residence;
+    private CheckBox  rented;
+    private Button    dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +27,16 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
         setContentView(R.layout.activity_residence);
 
         geolocation = (EditText) findViewById(R.id.geolocation);
-        residence = new Residence();
-
+        residence   = new Residence();
         geolocation.addTextChangedListener(this);
+
+        dateButton  = (Button)   findViewById(R.id.registration_date);
+        dateButton.setEnabled(false);
+
+        rented      = (CheckBox) findViewById(R.id.isrented);
+        rented.setOnCheckedChangeListener(this);
+
+
     }
 
     @Override
@@ -38,5 +52,11 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
     @Override
     public void afterTextChanged(Editable editable) {
         residence.setGeolocation(editable.toString());
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Log.i(this.getClass().getSimpleName(), "rented Checked");
+        residence.rented = isChecked;
     }
 }
