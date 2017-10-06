e84e065268a8dfe257b4cd312ddc4bdbf08fddda
Niall Coady <niall_coady7@hotmail.com>
2017-10-06 14:14:36 +0100

    Change to MyRentApp for Portfolio

--- app/src/main/java/com/example/myrent/app/MyRentApp.java
+++ app/src/main/java/com/example/myrent/app/MyRentApp.java
@@ -3,17 +3,19 @@ package com.example.myrent.app;
 
 import android.app.Application;
 import com.example.myrent.models.Portfolio;
+import com.example.myrent.models.PortfolioSerializer;
+
 import static com.example.myrent.android.helpers.LogHelpers.info;
 
-public class MyRentApp extends Application
-{
+public class MyRentApp extends Application {
+    private static final String FILENAME = "portfolio.json";
     public Portfolio portfolio;
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
-        portfolio = new Portfolio();
+        PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+        portfolio = new Portfolio(serializer);
 
         info(this, "MyRent app launched");
     }
