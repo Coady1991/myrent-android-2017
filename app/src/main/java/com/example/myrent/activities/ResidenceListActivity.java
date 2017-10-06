f4896bbe91e8fc3abec8643aeaf6d504a9ec8925
Niall Coady <niall_coady7@hotmail.com>
2017-10-06 11:45:55 +0100

    Added ResidenceAdapter

--- app/src/main/java/com/example/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/com/example/myrent/activities/ResidenceListActivity.java
@@ -3,18 +3,28 @@ package com.example.myrent.activities;
 import com.example.myrent.R;
 import com.example.myrent.app.MyRentApp;
 import com.example.myrent.models.Portfolio;
+import com.example.myrent.models.Residence;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ResidenceListActivity extends AppCompatActivity
-{
+import java.util.ArrayList;
+
+public class ResidenceListActivity extends AppCompatActivity {
     private ListView listView;
     private Portfolio portfolio;
+    private ResidenceAdapter adapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.app_name);
         setContentView(R.layout.activity_residence_list);
@@ -23,5 +33,38 @@ public class ResidenceListActivity extends AppCompatActivity
 
         MyRentApp app = (MyRentApp) getApplication();
         portfolio = app.portfolio;
+
+        adapter = new ResidenceAdapter(this, portfolio.residences);
+        listView.setAdapter(adapter);
+    }
+}
+
+class ResidenceAdapter extends ArrayAdapter<Residence>
+{
+    private Context context;
+
+    public ResidenceAdapter(Context context, ArrayList<Residence> residences) {
+        super(context, 0, residences);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_item_residence, null);
+        }
+        Residence res = getItem(position);
+
+        TextView geolocation = (TextView) convertView.findViewById(R.id.residence_list_item_geolocation);
+        geolocation.setText(res.geolocation);
+
+        TextView dateTextView = (TextView) convertView.findViewById(R.id.residence_list_item_dateTextView);
+        dateTextView.setText(res.getDateString());
+
+        CheckBox rentedCheckBox = (CheckBox) convertView.findViewById(R.id.residence_list_item_isrented);
+        rentedCheckBox.setChecked(res.rented);
+
+        return convertView;
     }
 }
